e2c445205cf6719b22b7c5c9eeab3fcb626f4eef
Lakshman Roy <121234657+lakshmanroy@users.noreply.github.com>
2022-12-25 14:15:36 +0530

    Add files via upload

---
+++ project.c
@@ -0,0 +1,35 @@
+// i have creat the random number game and attend choose your correct answer c language 
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int main()
+{
+
+   int number,guess,nguesses;
+   srand(time(0));
+   number=rand()%100+1;
+do
+{
+	//For random number generator in C
+	printf("guess the number 1 to 100\n");
+	scanf("%d",&guess);
+	if(guess>number)
+	{
+		printf("Lower number please\n");
+	}
+	else if(guess<number)
+	{
+		printf("Higer number please\n");
+	}
+	else
+	{
+		printf("you guesses number attempts %d is %d\n",nguesses);
+		printf("Thanku you are win");
+	}
+	nguesses++;
+}
+	while(guess!=number);
+	return 0;
+
+}
+
